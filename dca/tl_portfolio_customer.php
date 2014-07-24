86e40cdda37ce085ab348c5778c0761c5e7e1f73
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2014-07-24 18:31:37 +0430

    reduce logo size

--- dca/tl_portfolio_customer.php
+++ dca/tl_portfolio_customer.php
@@ -261,7 +261,7 @@ class tl_portfolio_customer extends Backend
 
 		if ($objImage !== null)
 		{
-			$strImage = \Image::getHtml(\Image::get($objImage->path, '60', '60', 'center_center'));
+			$strImage = \Image::getHtml(\Image::get($objImage->path, '30', '30', 'center_center'));
 		}
 
 		return '<div><div style="float:left; margin-right:10px;">'.$strImage.'</div>'. $arrRow['title'] . '</div>';
