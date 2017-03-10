314827395e07ae1258ce6f634d741afde479739b
Hamid Abbaszadeh <abbaszadeh.h@gmail.com>
2017-03-10 14:53:53 +0330

    Remove provinces string and add project headline

--- languages/en/default.php
+++ languages/en/default.php
@@ -18,40 +18,9 @@
 $GLOBALS['TL_LANG']['MSC']['emptyClientList']   = 'No client found!';
 $GLOBALS['TL_LANG']['MSC']['emptyProjectList']  = 'No project found!';
 $GLOBALS['TL_LANG']['MSC']['moredetail']        = 'More detail';
+$GLOBALS['TL_LANG']['MSC']['project_headline']  = 'Projects';
 
 
 $GLOBALS['TL_LANG']['MSC']['planed']            = 'Planed';
 $GLOBALS['TL_LANG']['MSC']['started']           = 'Started';
 $GLOBALS['TL_LANG']['MSC']['completed']         = 'Completed';
-
-$GLOBALS['TL_LANG']['MSC']['P01'] = 'آذربایجان شرقی';
-$GLOBALS['TL_LANG']['MSC']['P02'] = 'آذربایجان غربی';
-$GLOBALS['TL_LANG']['MSC']['P03'] = 'اردبیل';
-$GLOBALS['TL_LANG']['MSC']['P04'] = 'اصفهان';
-$GLOBALS['TL_LANG']['MSC']['P05'] = 'البرز';
-$GLOBALS['TL_LANG']['MSC']['P06'] = 'ایلام';
-$GLOBALS['TL_LANG']['MSC']['P07'] = 'بوشهر';
-$GLOBALS['TL_LANG']['MSC']['P08'] = 'تهران';
-$GLOBALS['TL_LANG']['MSC']['P09'] = 'چهارمحال و بختیاری';
-$GLOBALS['TL_LANG']['MSC']['P10'] = 'خراسان جنوبی';
-$GLOBALS['TL_LANG']['MSC']['P11'] = 'خراسان رضوی';
-$GLOBALS['TL_LANG']['MSC']['P12'] = 'خراسان شمالی';
-$GLOBALS['TL_LANG']['MSC']['P13'] = 'خوزستان';
-$GLOBALS['TL_LANG']['MSC']['P14'] = 'زنجان';
-$GLOBALS['TL_LANG']['MSC']['P15'] = 'سمنان';
-$GLOBALS['TL_LANG']['MSC']['P16'] = 'سیستان و بلوچستان';
-$GLOBALS['TL_LANG']['MSC']['P17'] = 'فارس';
-$GLOBALS['TL_LANG']['MSC']['P18'] = 'قزوین';
-$GLOBALS['TL_LANG']['MSC']['P19'] = 'قم';
-$GLOBALS['TL_LANG']['MSC']['P20'] = 'کردستان';
-$GLOBALS['TL_LANG']['MSC']['P21'] = 'کرمان';
-$GLOBALS['TL_LANG']['MSC']['P22'] = 'کرمانشاه';
-$GLOBALS['TL_LANG']['MSC']['P23'] = 'کهگیلویه و بویراحمد';
-$GLOBALS['TL_LANG']['MSC']['P24'] = 'گلستان';
-$GLOBALS['TL_LANG']['MSC']['P25'] = 'گیلان';
-$GLOBALS['TL_LANG']['MSC']['P26'] = 'لرستان';
-$GLOBALS['TL_LANG']['MSC']['P27'] = 'مازنداران';
-$GLOBALS['TL_LANG']['MSC']['P28'] = 'مرکزی';
-$GLOBALS['TL_LANG']['MSC']['P29'] = 'هرمزگان';
-$GLOBALS['TL_LANG']['MSC']['P30'] = 'همدان';
-$GLOBALS['TL_LANG']['MSC']['P31'] = 'یزد';
